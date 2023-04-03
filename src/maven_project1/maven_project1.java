8f44b92eea6682783bb0d22a5d2f93acf2780c2f
kailash kadu <55619119+kailashkadu@users.noreply.github.com>
2023-04-03 08:35:08 +0530

    Update maven_project1.java

--- src/maven_project1/maven_project1.java
+++ src/maven_project1/maven_project1.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class maven_project1 {
-
+        //this is main method
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com");
